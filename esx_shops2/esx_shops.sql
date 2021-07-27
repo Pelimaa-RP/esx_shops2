a22712dba6ff32f4c9cec08e72fe5c08ea33b1b4
Pelimaa-RP <88039777+Pelimaa-RP@users.noreply.github.com>
2021-07-27 22:12:17 +0300

    Update esx_shops.sql

--- esx_shops2/esx_shops.sql
+++ esx_shops2/esx_shops.sql
@@ -1,6 +1,6 @@
 USE `es_extended`;
 
-CREATE TABLE `shops` (
+CREATE TABLE `shops2` (
 	`id` int NOT NULL AUTO_INCREMENT,
 	`store` varchar(100) NOT NULL,
 	`item` varchar(100) NOT NULL,
@@ -9,11 +9,14 @@ CREATE TABLE `shops` (
 	PRIMARY KEY (`id`)
 );
 
-INSERT INTO `shops` (store, item, price) VALUES
-	('TwentyFourSeven','bread',30),
-	('TwentyFourSeven','water',15),
-	('RobsLiquor','bread',30),
-	('RobsLiquor','water',15),
-	('LTDgasoline','bread',30),
-	('LTDgasoline','water',15)
+INSERT INTO `shops2` (store, item, price) VALUES
+	('TwentyFourSeven','silencer',300),
+	('TwentyFourSeven','bulletproof',1000),
+	('TwentyFourSeven','grip',150),
+	('RobsLiquor','silencer',300),
+	('RobsLiquor','bulletproof',1000),
+	('RobsLiquor','grip',150),
+	('LTDgasoline','silencer',300),
+	('LTDgasoline','bulletproof',1000),
+	('LTDgasoline','grip',150)
 ;
