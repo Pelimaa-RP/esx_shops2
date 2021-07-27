1f5cd4bf2875eb054f81b5427444fd0551cf1f44
Pelimaa-RP <88039777+Pelimaa-RP@users.noreply.github.com>
2021-07-27 22:01:02 +0300

    Add files via upload

---
+++ esx_shops2/config.lua
@@ -0,0 +1,40 @@
+Config              = {}
+Config.DrawDistance = 1.5
+Config.Draw2Distance = 3.6
+Config.Size         = {x = 0.5, y = 0.5, z = 0.5}
+Config.Color        = {r = 60, g = 129, b = 198}
+Config.Type         = 32
+Config.Locale       = 'fi'
+
+Config.Map = {
+	
+
+
+	
+	
+}
+
+Config.Zones = {
+
+	TwentyFourSeven = {
+		Items = {},
+		Pos = {
+			{x = 1966.98,   y = 4634.36,  z = 41.1}
+			
+		}
+	},
+
+	RobsLiquor = {
+		Items = {},
+		Pos = {
+			
+		}
+	},
+
+	LTDgasoline = {
+		Items = {},
+		Pos = {
+		
+		}
+	}
+}
